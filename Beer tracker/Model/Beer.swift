cdc1067c5d431191c5401373d4941b860ccc2614
Michiel Glibert <michiel.glibert@gmail.com>
2017-12-26 16:54:31 +0100

    UI Changes

--- Beer tracker/Model/Beer.swift
+++ Beer tracker/Model/Beer.swift
@@ -1,1 +1,22 @@
-
+class Beer {
+    
+    enum Kleur: String {
+        case blond = "Blond"
+        case blondbruin = "Blond/Bruin"
+        case bruin = "Bruin"
+    }
+    
+    var naam:String
+    var brouwer:String
+    var kleur:Kleur
+    var alcoholPercentage:Double
+    var rating:Int
+    
+    init(naam: String, brouwer: String, kleur: Kleur, alcoholPercentage: Double, rating: Int) {
+        self.naam = naam
+        self.brouwer = brouwer
+        self.kleur = kleur
+        self.alcoholPercentage = alcoholPercentage
+        self.rating = rating
+    }
+}

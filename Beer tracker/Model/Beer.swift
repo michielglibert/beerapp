f9be2f826ac2c673b611fdd6e2ef0f5efb68f083
Michiel Glibert <michiel.glibert@gmail.com>
2017-12-26 19:40:11 +0300

    Tab controller toegevoegd

--- Beer tracker/Model/Beer.swift
+++ Beer tracker/Model/Beer.swift
@@ -11,12 +11,14 @@ class Beer {
     var kleur:Kleur
     var alcoholPercentage:Double
     var rating:Int
+    var favoriet:Bool
     
-    init(naam: String, brouwer: String, kleur: Kleur, alcoholPercentage: Double, rating: Int) {
+    init(naam: String, brouwer: String, kleur: Kleur, alcoholPercentage: Double, rating: Int, favoriet: Bool) {
         self.naam = naam
         self.brouwer = brouwer
         self.kleur = kleur
         self.alcoholPercentage = alcoholPercentage
         self.rating = rating
+        self.favoriet = favoriet
     }
 }
